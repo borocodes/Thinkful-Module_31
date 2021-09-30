d880068282bca3da141f178b87e0af3b8c549b1b
Jonathan Borofsky <jonborofsky@gmail.com>
2021-09-29 19:26:39 -0500

    Mod 31.3 RESTful API Lesson

--- src/app.js
+++ src/app.js
@@ -1,7 +1,62 @@
 const express = require("express");
 const app = express();
 
+
 // TODO: Follow instructions in the checkpoint to implement ths API.
+
+const flips = require("./data/flips-data");
+const counts = require("./data/counts-data");
+
+app.use(express.json());
+
+app.use("/counts/:countId", (req, res, next) => {
+  const { countId } = req.params;
+  const foundCount = counts[countId];
+
+  if (foundCount === undefined) {
+    next(`Count id not found: ${countId}`);
+  } else {
+    res.json({ data: foundCount }); // Return a JSON object, not a number.
+  }
+});
+
+app.use("/counts", (req, res) => {
+  res.json({ data: counts });
+});
+
+app.use("/flips/:flipId", (req, res, next) => {
+  const { flipId } = req.params;
+  const foundFlip = flips.find((flip) => flip.id === Number(flipId));
+
+  if (foundFlip) {
+    res.json({ data: foundFlip });
+  } else {
+    next(`Flip id not found: ${flipId}`);
+  }
+});
+
+app.get("/flips", (req, res) => {
+  res.json({ data: flips });
+});
+
+// Variable to hold the next ID
+// Because some IDs may already be used, find the largest assigned ID
+let lastFlipId = flips.reduce((maxId, flip) => Math.max(maxId, flip.id), 0);
+
+app.post("/flips", (req, res, next) => {
+  const { data: { result } = {} } = req.body;
+  if (result) {
+    const newFlip = {
+      id: ++lastFlipId, // Increment last ID, then assign as the current ID
+      result,
+    };
+    flips.push(newFlip);
+    counts[result] = counts[result] + 1; // Increment the counts
+    res.status(201).json({ data: newFlip });
+  } else {
+    res.sendStatus(400);
+  }
+});
 
 // Not found handler
 app.use((request, response, next) => {
